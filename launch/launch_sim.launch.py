ea8d2299e9efa67da74d10b3ee2ed6163b47741d
adrianCo <adrian.cobo.mh@gmail.com>
2023-06-24 17:10:09 +0200

    depth camera and ros2_controll config added
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -36,9 +36,23 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+    )
+
     # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        diff_drive_spawner,
+        joint_broad_spawner
     ])
