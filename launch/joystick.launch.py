5874b25ada2b91343435c46c8ff8e6706cce9745
adrianCo <adrian.cobo.mh@gmail.com>
2023-06-27 16:52:06 +0200

    navigation with nav2 added
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         name='teleop_node',
         parameters=[joy_params],
         # remap necessary for ros2_control node
-        remappings=[('/cmd_vel', '/diff_cont/cmd_vel_unstamped')]
+        remappings=[('/cmd_vel', '/cmd_vel_joy')]
     )
 
     return LaunchDescription([
--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -24,6 +24,20 @@ def generate_launch_description():
         )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    joystick = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'joystick.launch.py')]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    # publish msgs from joystic topic (prio 1) and cmd_vels(prio 2) in /diff_cont/cmd_vel_unstamped
+    twist_mux_params = os.path.join(get_package_share_directory(
+        package_name), 'config', 'twist_mux.yaml')
+    twist_mux = Node(package='twist_mux',
+                     executable='twist_mux',
+                     parameters=[twist_mux_params, {'use_sim_time': True}],
+                     remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')])
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
@@ -51,6 +65,8 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
+        twist_mux,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
