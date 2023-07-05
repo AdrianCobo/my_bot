caf2ea3e77f15f16933e3c25fcc7e71594d6d5d1
adrianCo <adrian.cobo.mh@gmail.com>
2023-07-05 11:18:43 +0200

    launcher error corrected v2
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
-                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000: 01: 00.0-usb-0: 1.4: 1.0-port0',
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.4:1.0-port0',
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
