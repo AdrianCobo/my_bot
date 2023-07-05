5d0eae1196414bdb2240be3458a032325499c214
adrianCo <adrian.cobo.mh@gmail.com>
2023-07-05 11:17:17 +0200

    launcher error corrected
    Signed-off-by: adrianCo <adrian.cobo.mh@gmail.com>

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000: 01: 00.0-usb-0: 1.4: 1.0-port0',
                 'frame_id': 'laser_frame',
-                'angle_compensate': 'true',
+                'angle_compensate': True,
                 'scan_mode': 'Standard'
             }],
         )
