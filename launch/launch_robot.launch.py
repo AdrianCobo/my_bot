c7d58930d3a67822c1454098523adbb11925b5fa
AdrianCobo <adrian.cobo.mh@gmail.com>
2023-08-16 19:49:31 +0200

    c
    Signed-off-by: AdrianCobo <adrian.cobo.mh@gmail.com>

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -13,23 +13,42 @@ from launch.event_handlers import OnProcessStart
 from launch_ros.actions import Node
 
 
+
 def generate_launch_description():
+
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name = 'my_bot'  # <--- CHANGE ME
+    package_name='my_bot' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(
-                package_name), 'launch', 'real_robot_spawner.launch.py'
-        )]), launch_arguments={'use_sim_time': 'false'}.items()
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    # joystick = IncludeLaunchDescription(
+    #             PythonLaunchDescriptionSource([os.path.join(
+    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
+    #             )])
+    # )
+
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
     
-    controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
+
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+
+    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
@@ -66,9 +85,30 @@ def generate_launch_description():
         )
     )
 
+
+    # Code for delaying a node (I haven't tested how effective it is)
+    # 
+    # First add the below lines to imports
+    # from launch.actions import RegisterEventHandler
+    # from launch.event_handlers import OnProcessExit
+    #
+    # Then add the following below the current diff_drive_spawner
+    # delayed_diff_drive_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=spawn_entity,
+    #         on_exit=[diff_drive_spawner],
+    #     )
+    # )
+    #
+    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+
+
+
     # Launch them all!
     return LaunchDescription([
         rsp,
+        # joystick,
+        twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
