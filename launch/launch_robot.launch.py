28206366fdd666b8291ef9ba9e7fe639f92eb512
AdrianCobo <adrian.cobo.mh@gmail.com>
2023-08-16 19:27:05 +0200

    c
    Signed-off-by: AdrianCobo <adrian.cobo.mh@gmail.com>

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -27,20 +27,6 @@ def generate_launch_description():
         )]), launch_arguments={'use_sim_time': 'false'}.items()
     )
 
-    joystick = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'joystick.launch.py')]),
-        launch_arguments={'use_sim_time': 'false'}.items()
-    )
-
-    # publish msgs from joystic topic (prio 1) and cmd_vels(prio 2) in /diff_cont/cmd_vel_unstamped
-    twist_mux_params = os.path.join(get_package_share_directory(
-        package_name), 'config', 'twist_mux.yaml')
-    twist_mux = Node(package='twist_mux',
-                     executable='twist_mux',
-                     parameters=[twist_mux_params, {'use_sim_time': 'false'}],
-                     remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')])
-
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
     
     controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml')
@@ -83,8 +69,6 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
-        joystick,
-        twist_mux,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
