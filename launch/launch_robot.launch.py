a56f1951b93e439c9850761eee46120aa4dbe21f
AdrianCobo <adrian.cobo.mh@gmail.com>
2023-08-16 19:25:19 +0200

    c
    Signed-off-by: AdrianCobo <adrian.cobo.mh@gmail.com>

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -8,7 +8,7 @@ from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch.actions import RegisterEventHandler
-from launch.event_handler import OnProcessStart
+from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
