60496c03f9d21ea8e046f34cd3d74a93a2dab178
AdrianCobo <adrian.cobo.mh@gmail.com>
2023-08-18 12:24:12 +0200

    minor_changes
    Signed-off-by: AdrianCobo <adrian.cobo.mh@gmail.com>

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                    get_package_share_directory(package_name),'launch','real_robot_spawner.launch.py'
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
