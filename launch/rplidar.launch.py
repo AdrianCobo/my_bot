952c14692810df428e648e3b47c383418fbf940e
AdrianCobo <adrian.cobo.mh@gmail.com>
2023-08-25 09:50:20 +0200

    rplidar.launch Actualized
    Signed-off-by: AdrianCobo <adrian.cobo.mh@gmail.com>

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -10,11 +10,15 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='rplidar_ros',
-            executable='rplidar_composition',
+            executable='rplidar_node',
             output='screen',
+            name='rplidar_node',
             parameters=[{
-                'serial_port': '/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0',
+                'channel_type': 'serial',
+                'serial_port': '/dev/ttyUSB0',
                 'frame_id': 'laser_frame',
+                'inverted': False,
+                'serial_baudrate': 115200,
                 'angle_compensate': True,
                 'scan_mode': 'Standard'
             }],
