7716a8a6a1da472d4c3447d18bdb692fa67917f6
adrianco <adrian.cobo.mh@gmail.com>
2024-09-21 20:24:47 +0200

    my_controller
    Signed-off-by: adrianco <adrian.cobo.mh@gmail.com>

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -48,15 +48,20 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
     xacro_file = os.path.join(pkg_path, 'description', 'real_robot.urdf.xacro')
-    robot_description = Command(['xacro ', xacro_file, ' use_ros2_control:=', 'True', ' sim_mode:=', 'False'])
+    robot_description_content = Command(['xacro ', xacro_file, ' use_ros2_control:=', 'True', ' sim_mode:=', 'False'])
+    robot_description = {"robot_description": robot_description_content}
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': launch_ros.descriptions.ParameterValue(robot_description, value_type=str)},
-                    controller_params_file]
+        parameters=[controller_params_file],
+        output="both",
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+            #("diff_cont/cmd_vel_unstamped", "/cmd_vel"),
+        ],
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
@@ -64,7 +69,7 @@ def generate_launch_description():
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["diff_cont"],
+        arguments=["diff_cont", "--param-file", controller_params_file],
     )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
